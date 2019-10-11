f10e5da71df33b5320ef556ea0959d2f257adcc8
Geoff Blair <geoff@mousepox.io>
2019-10-10 20:27:00 -0600

    Add copy, constrain, normalize, subtract, and magnitude to Vector2

--- lib/Vector2.ts
+++ lib/Vector2.ts
@@ -1,4 +1,4 @@
-import { IPoint } from "./core";
+import { clamp, IPoint } from "./core";
 
 /** A vector in 2D space */
 export class Vector2 implements IPoint {
@@ -19,6 +19,11 @@ export class Vector2 implements IPoint {
     return Math.atan2(this.y, this.x);
   }
 
+  /** Mangitude (or length of this vector) */
+  public get magnitude(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
   constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
@@ -34,10 +39,46 @@ export class Vector2 implements IPoint {
     return this;
   }
 
+  /**
+   * Copy the components of another vector
+   * @chainable
+   */
+  public copy(v: Vector2): Vector2 {
+    this.x = v.x;
+    this.y = v.y;
+    return this;
+  }
+
+  /**
+   * Normalize this vector
+   * @chainable
+   */
+  public normalize(): Vector2 {
+    const mag = this.magnitude;
+    if (mag === 0) {
+      return this;
+    } else {
+      return this.scale(1 / mag);
+    }
+  }
+
   /** Scale this vector by a given scalar */
   public scale(n: number): Vector2 {
     this.x *= n;
     this.y *= n;
+    return this;
+  }
+
+  public subtract(v: Vector2): Vector2 {
+    this.x -= v.x;
+    this.y -= v.y;
+    return this;
+  }
+
+  /** Constrain this vector's components by specified bounds */
+  public constrain(x: number, y: number, width: number, height: number): Vector2 {
+    this.x = clamp(this.x, x, x + width);
+    this.y = clamp(this.y, y, y + height);
     return this;
   }
 
